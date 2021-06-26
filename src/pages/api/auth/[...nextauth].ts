ee816d61beec7097533e791ace3d308ecf604eae
Deeptesh Chagan <deeptesh.chagan@austcommunitymedia.com.au>
2021-06-26 13:00:03 +1000

    add some auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  providers: [
+    Providers.Cognito({
+      clientId: process.env.COGNITO_CLIENT_ID,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET,
+      domain: process.env.COGNITO_DOMAIN,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+});
