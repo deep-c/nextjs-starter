5d3b92377554bd434f2164fdab26eef50fbddee2
Deeptesh Chagan <deeptesh.chagan@austcommunitymedia.com.au>
2021-06-30 23:48:07 +1000

    commit pascal casing

---
+++ src/components/Auth.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/client';
+import type { Session } from 'next-auth';
+import type { Role } from '@prisma/client';
+import type { NextPage } from 'next';
+import type { UrlObject } from 'url';
+import routes from '@/routes';
+
+export interface AuthProps {
+  loginUrl?: UrlObject | string;
+  allowedRoles?: Role[];
+}
+
+export type NextAuthPage<P> = NextPage<P, P> & {
+  auth?: AuthProps;
+  layout?: ReactNode;
+};
+
+export const roleHasAccess = (session: Session, roles: Role[]) => {
+  return !!roles.find((role) => role === session?.user?.role);
+};
+
+const Auth: React.FC<AuthProps> = ({ children, loginUrl, allowedRoles }) => {
+  const [session, loading] = useSession();
+  const hasUser = !!session?.user;
+  const router = useRouter();
+  useEffect(() => {
+    if (!loading) {
+      if (!hasUser) {
+        router.push(loginUrl ?? routes.APP.LOGIN);
+      } else if (
+        session &&
+        allowedRoles &&
+        !roleHasAccess(session, allowedRoles)
+      ) {
+        router.push(routes.UNAUTHORIZED);
+      }
+    }
+    if (!loading && !hasUser && loginUrl) {
+      router.push(loginUrl);
+    }
+  }, [loading, hasUser, router, loginUrl, allowedRoles, session]);
+  if (loading || !hasUser) {
+    return <div />;
+  }
+  return <>{children}</>;
+};
+
+Auth.defaultProps = {
+  loginUrl: routes.APP.LOGIN,
+};
+
+export default Auth;
