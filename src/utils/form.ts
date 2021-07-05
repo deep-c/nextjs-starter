8881de5a8355a99c0ac727bde57e8aba3f0ecdfc
Deeptesh Chagan <deeptesh.chagan@fairfaxmedia.com.au>
2021-07-05 16:49:27 +1000

    remove unneeded

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -4,15 +4,12 @@ export const getDirtyValues = (
   dirtyFields: UnknownArrayOrObject | boolean,
   allValues: UnknownArrayOrObject
 ): UnknownArrayOrObject => {
-  // NOTE: Recursive function.
-
   // If *any* item in an array was modified, the entire array must be submitted, because there's no
   // way to indicate "placeholders" for unchanged elements. `dirtyFields` is `true` for leaves.
   if (dirtyFields === true || Array.isArray(dirtyFields)) {
     return allValues;
   }
 
-  // Here, we have an object.
   return Object.fromEntries(
     Object.keys(dirtyFields).map((key) => [
       key,
