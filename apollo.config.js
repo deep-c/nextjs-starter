83f3123f17cd61b7d251d9d851ad5c66bb730d2a
Deeptesh Chagan <deeptesh.chagan@fairfaxmedia.com.au>
2021-07-07 20:54:47 +1000

    use apollo config file

---
+++ apollo.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'spa-graphql',
+      // Only exists when npm run build:nexus or npm run dev is run and a GraphQL request made.
+      localSchemaFile: './src/types/__generated__/nexus/schema.graphql',
+    },
+    excludes: ['**/__tests__/**/*', '**/schema.graphql'],
+    tagName: 'gql',
+  },
+};
