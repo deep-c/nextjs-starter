94adb8a8d18223abeec9f29c9307521582317f1d
Deeptesh Chagan <deeptesh.chagan@fairfaxmedia.com.au>
2021-07-09 12:12:44 +1000

    give unauthorized admin layout

--- src/pages/unauthorized.tsx
+++ src/pages/unauthorized.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import type { NextPage } from 'next';
+import { BanIcon } from '@heroicons/react/solid';
+import { UNAUTHORIZED, NextRoutePage } from '@/routes';
+import AdminLayout from '@/layouts/admin';
 
-const UnAuthorized: NextPage = () => {
+const UnAuthorized: NextRoutePage<{}> = () => {
   return (
-    <h1>
-      Unauthorized - You do not have the priviledges required to view the page.
-    </h1>
+    <div className="rounded-md bg-red-50 p-4 mx-auto mt-6">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <BanIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-red-800">
+            UNAUTHORIZED - You do not have permission to view this page.
+          </h3>
+        </div>
+      </div>
+    </div>
   );
 };
+
+UnAuthorized.auth = UNAUTHORIZED.auth;
+UnAuthorized.layout = AdminLayout;
 
 export default UnAuthorized;
