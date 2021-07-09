7526359b10301ad76c54e47563ac45e1d641c03f
Deeptesh Chagan <deeptesh.chagan@fairfaxmedia.com.au>
2021-07-09 13:37:41 +1000

    correct typing error

--- src/types/node.d.ts
+++ src/types/node.d.ts
@@ -1,3 +1,18 @@
 declare namespace NodeJS {
   interface Global {}
+  // See .env.local.example. This should be kept in sync with variables added and removed
+  interface ProcessEnv {
+    APP_HOSTNAME: string;
+    PORT?: string;
+    NEXT_PUBLIC_HOST: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_USER: string;
+    POSTGRES_DB: string;
+    DATABASE_URL: string;
+    NEXTAUTH_URL: string;
+    COGNITO_CLIENT_ID: string;
+    COGNITO_CLIENT_SECRET: string;
+    COGNITO_DOMAIN: string;
+    DEBUG: string;
+  }
 }
