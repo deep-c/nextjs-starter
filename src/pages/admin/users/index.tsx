860d37efa97d7dfeb5e2895706b0d01ff4c4e3b1
Deeptesh Chagan <deeptesh.chagan@fairfaxmedia.com.au>
2021-07-31 12:19:57 +1000

    add search field

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client';
+import { SearchIcon } from '@heroicons/react/outline';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
@@ -30,6 +31,31 @@ export const UsersAdmin: NextRoutePage<UsersAdminProps> = () => {
       </Head>
       <div className="pt-6 pb-6">
         <h1 className="text-3xl font-extrabold text-gray-900">Manage Users</h1>
+      </div>
+      <div className="flex flex-row-reverse">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Quick Search
+          </label>
+          <div className="mt-1 relative rounded-md shadow-sm">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </div>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Search name or email.."
+            />
+          </div>
+        </div>
       </div>
       <div className="mt-4 flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
